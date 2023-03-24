4db3d939743a1ac35fa9fc4168844e9cb227f9ab
Bricecy <brigidwambua97@gmail.com>
2023-03-24 15:49:40 -0700

    Updated file

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-for file in *.c; do
-gcc -fPIC -c "$file"
-done
-gcc -shared -o libdynamic.so *.o
-rm *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -shared -fPIC -o liball.so *.c
